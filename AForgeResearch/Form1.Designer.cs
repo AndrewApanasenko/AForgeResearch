e7783bfd20ed267949875cd2a291817b9e6597ab
AndrewApanasenko <sale@xmgx.ru>
2021-03-10 22:39:29 +0300

    v0.1 add play\stop

--- AForgeResearch/Form1.Designer.cs
+++ AForgeResearch/Form1.Designer.cs
@@ -28,13 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.lstDevice = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnPlayPause = new System.Windows.Forms.ToolStripButton();
+            this.btnStop = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.scrCapture = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scrCapture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.lstDevice,
+            this.toolStripSeparator1,
+            this.btnPlayPause,
+            this.btnStop,
+            this.toolStripSeparator2,
+            this.btnExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(553, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(109, 22);
+            this.toolStripLabel1.Text = "Video Input Device:";
+            // 
+            // lstDevice
+            // 
+            this.lstDevice.FlatStyle = System.Windows.Forms.FlatStyle.Standard;
+            this.lstDevice.Name = "lstDevice";
+            this.lstDevice.Size = new System.Drawing.Size(121, 25);
+            this.lstDevice.SelectedIndexChanged += new System.EventHandler(this.lstDevice_SelectedIndexChanged);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnPlayPause.Image = global::AForgeResearch.Properties.Resources.play;
+            this.btnPlayPause.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(23, 22);
+            this.btnPlayPause.Text = "toolStripButton1";
+            this.btnPlayPause.ToolTipText = "Play";
+            this.btnPlayPause.Click += new System.EventHandler(this.btnPlayPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnStop.Image = global::AForgeResearch.Properties.Resources.stop;
+            this.btnStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(23, 22);
+            this.btnStop.Text = "toolStripButton1";
+            this.btnStop.ToolTipText = "Stop";
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnExit.Image = global::AForgeResearch.Properties.Resources.exit;
+            this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(23, 22);
+            this.btnExit.Text = "toolStripButton1";
+            this.btnExit.ToolTipText = "Exit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // scrCapture
+            // 
+            this.scrCapture.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("scrCapture.BackgroundImage")));
+            this.scrCapture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.scrCapture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scrCapture.Location = new System.Drawing.Point(0, 25);
+            this.scrCapture.Name = "scrCapture";
+            this.scrCapture.Size = new System.Drawing.Size(553, 400);
+            this.scrCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.scrCapture.TabIndex = 1;
+            this.scrCapture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(553, 425);
+            this.Controls.Add(this.scrCapture);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "AForge Research [Just testing]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scrCapture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox lstDevice;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnPlayPause;
+        private System.Windows.Forms.ToolStripButton btnStop;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnExit;
+        private System.Windows.Forms.PictureBox scrCapture;
     }
 }
 
--- AForgeResearch/Form1.cs
+++ AForgeResearch/Form1.cs
@@ -7,14 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video.DirectShow;
 
 namespace AForgeResearch
 {
     public partial class Form1 : Form
     {
+        private FilterInfoCollection devices = null;
+        private VideoCaptureDevice captureDevice = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo dev in devices)
+            {
+                lstDevice.Items.Add(dev.Name);
+            }
+        }
+
+        private void lstDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            captureDevice = new VideoCaptureDevice(devices[lstDevice.SelectedIndex].MonikerString);
+        }
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (devices.Count == 0)
+                {
+                    throw new Exception("No devices found!");
+                }
+                else if (lstDevice.SelectedItem == null)
+                {
+                    throw new Exception("Device not select!");
+                }
+                else
+                {
+                    captureDevice.NewFrame += CaptureDevice_NewFrame;
+                    captureDevice.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CaptureDevice_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            scrCapture.Image = new Bitmap(eventArgs.Frame);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            captureDevice.Stop();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
+
 }
